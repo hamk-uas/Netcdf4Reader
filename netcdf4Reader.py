c8cff7982fdaf6a5b6e2a60b5fa05a48a8a46271
jskodova <skodovajarmilatt@gmail.com>
2023-06-15 23:06:57 +0300

    adding splicing functionality for netcdf and saving it to csv

--- netcdf4Reader.py
+++ netcdf4Reader.py
@@ -6,6 +6,20 @@ import os
 
 s3 = boto3.client('s3', aws_access_key_id='youraccessid', aws_secret_access_key='yoursecretkey')
 
+# Setting the measurand and year
+measurand = "tday"
+year = 2005
+
+# Setting the time range
+start_time = "2005-01-01"
+end_time = "2005-01-02"
+
+# Setting the longitude and latitude range
+start_longitude = 7.319e+04
+end_longitude = 7.332e+05
+start_latitude = 7.777e+06
+end_latitude = 6.631e+06
+
 # Geting file from AWS
 def get_data(measurand, year):
     # Bucket and file
@@ -39,3 +53,19 @@ def describe_data(data):
     print(data[measurand.capitalize()])
     #looking at units
     print(data[measurand.capitalize()].attrs["units"])
+def read_data(data, start_time, end_time, start_longitude, end_longitude, start_latitude, end_latitude):
+    # Select slice based on time, longitude, and latitude
+    data_slice = data.sel(Time=slice(start_time, end_time), Lon=slice(start_longitude, end_longitude), Lat=slice(start_latitude, end_latitude))
+
+    # Convert 'Measurand' variable to a DataFrame
+    df = data_slice[measurand.capitalize()].to_dataframe(name=measurand.capitalize())
+    df = df.dropna()
+    print(df)
+    df.to_csv(f"{measurand}_{year}.csv")
+
+    # Convert 'Measurand' variable to a DataFrame
+    measurand_array = data_slice[measurand.capitalize()].values
+
+data= get_data(measurand, year)
+describe_data(data)
+read_data(data, start_time, end_time, start_longitude, end_longitude, start_latitude, end_latitude)
