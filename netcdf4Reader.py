a0dd6353c7a2221282232911e4c25ab26450b23e
jskodova <skodovajarmilatt@gmail.com>
2023-06-15 23:04:24 +0300

    import statements and function for sending the request to aws and receiving the netcdf4 file

---
+++ netcdf4Reader.py
@@ -0,0 +1,33 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import boto3
+import os
+
+s3 = boto3.client('s3', aws_access_key_id='youraccessid', aws_secret_access_key='yoursecretkey')
+
+# Geting file from AWS
+def get_data(measurand, year):
+    # Bucket and file
+    bucket_name = 'fmi-gridded-obs-daily-1km'
+    file_name = f'{measurand}_{year}.nc'
+
+    # S3 object path
+    s3_path = f'Netcdf/{measurand.capitalize()}/{file_name}'
+
+    try:
+        # Requesting file from S3
+        response = s3.get_object(Bucket=bucket_name, Key=s3_path)
+
+        # Saving response as a file
+        with open(file_name, 'wb') as f:
+            f.write(response['Body'].read())
+
+        # Opening the file with xarray
+        data = xr.open_dataset(file_name)
+
+        return data
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
